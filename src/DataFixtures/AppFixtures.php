3d14e22338e33c2079e2d59d2a463756baea8edc
YouriEmmanuel <youri.emmanuel@iut-dijon.u-bourgogne.fr>
2024-10-06 20:56:46 +0200

    new fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,9 @@ namespace App\DataFixtures;
 
 use App\Entity\Movie;
 use App\Entity\User;
+use App\Entity\Category;
+use App\Entity\Language;
+use App\Enum\MediaTypeEnum;
 use App\Enum\UserAccountStatusEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -14,13 +17,39 @@ class AppFixtures extends Fixture
     {
         for ($i = 0; $i < 5; $i++) {
             $movie = new Movie();
-            $movie ->setTitle("Star Wars ". $i);
-            $movie ->setShortDescription("blablaba");
-            $movie ->setLongDescription("blaaaaaaaaaaaaaaaaaaaaablaaaaaaaaaaaaaaaaaaaaaablaaaaaaaaaaaaaa");
-            $movie ->setCoverImage("star_wars". $i .".png");
-            $movie ->setRealeaseDateAt(new \DateTime("+. $i. day"));
+            $movie ->setTitle("Film ". $i);
+            $movie ->setShortDescription("Description du film ". $i);
+            $movie ->setLongDescription("Longue description du film ". $i);
+            $movie ->setCoverImage("film". $i .".png");
+            $movie ->setRealeaseDateAt(new \DateTime());
+            $movie -> setMediaType(MediaTypeEnum::MOVIE);
             $manager->persist($movie);
         }
+
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User() ;
+            $user ->setUsername("User" . $i);
+            $user ->setEmail("blabla".$i."@example.com");
+            $user ->setPassword("mdp");
+            $user->setAccountStatus(UserAccountStatusEnum::ACTIVE);
+            $manager->persist($user);
+        }
+
+        $category = [
+            ['Action', 'action'],
+            ['Aventure', 'aventure'],
+            ['Comédie', 'comédie'],
+            ['Drame', 'drame'],
+            ['Science-fiction', 'sci-Fi']
+        ];
+
+        foreach ($category as $categoryData) {
+            $category = new Category();
+            $category->setName($categoryData[0]);
+            $category->setLabel($categoryData[1]);
+            $manager->persist($category);
+        }
+
 
         $manager->flush();
     }
